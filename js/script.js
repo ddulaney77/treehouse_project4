c8a096034d61d5c7cd71785f6cd9137ab278e4af
ddulaney77 <31421351+ddulaney77@users.noreply.github.com>
2018-01-29 22:54:54 -0800

    adding js for search bar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-$(".search").on('click',function() {
-var filter = $( this).val().toLowerCase();
 
 
+//filter
 
 
+$(."search").on("keyup", function () {
+  var filter =$(this).val().toLowerCase();
+
+
+  $(."example-light").each( function () {
+
+if ( $(this) ).attr('title').toLowerCase().search( new RegExp(filter))<0
+  $(this).fadeOut();
+} else {
+  $(this).show();
 }
+});
+
+});
