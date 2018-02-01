e8d62a85895d7db3c19a32465b7c3a849d21070d
ddulaney77 <31421351+ddulaney77@users.noreply.github.com>
2018-02-01 08:42:12 -0800

    working on jquery issues

--- js/script.js
+++ js/script.js
@@ -1,24 +1,15 @@
 $(document).ready(function() {
-  $(".gallery").lightbox-plus-jquery ({
-    delegate: 'a',
-    type:'image',
-
-    gallery: {
-      enabled:true
-    }
-  })
-})
-
 
 
 $(".search").on("keyup", function() {
-  const input =$(this).val().toLowerCase();
+  var input = $(this).val().toLowerCase();
   $(".example-light").each(function() {
 
-if ( $(this) ).attr("data-title").toLowerCase().search(new regExp(filter))< 0){
+if ( $(this) ).attr("data-title").toLowerCase().search(input)< 0){
   $(this).hide();
 } else {
   $(this).show();
 }
 });
 });
+  });
