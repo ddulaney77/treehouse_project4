4bc80e69847aad04f4bca9da6b123dfb95883b74
ddulaney77 <31421351+ddulaney77@users.noreply.github.com>
2018-02-01 16:02:24 -0800

    fixing and finishing jquery search bar

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(".search").on("keyup", function() {
   var input = $(this).val().toLowerCase();
   $(".example-light").each(function() {
 
-if ( $(this) ).attr("data-title").toLowerCase().search(input)< 0){
+if ( $(this).attr("data-title").toLowerCase().search(input)< 0){
   $(this).hide();
 } else {
   $(this).show();
